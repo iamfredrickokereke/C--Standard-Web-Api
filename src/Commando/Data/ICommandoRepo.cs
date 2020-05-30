1b5ee09c52cb610518ae60ba676e20ae2947bacc
Kelly online <kellyonline2016@gmail.com>
2020-05-30 10:38:39 +0100

    added new repo

--- src/Commando/Data/ICommandoRepo.cs
+++ src/Commando/Data/ICommandoRepo.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+using Commando.Models;
+
 namespace Commando.Data
 {
     public interface ICommandoRepo
     {
-        
+        IEnumerable<Command> GetAppCommands();
+        Command GetCommandById(int id);
     }
 }
